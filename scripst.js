e990b19eb2885d2d1e93e29e04ba26623d8d8869
Julio Ochoa <jochoam8@est.ups.edu.ec>
2025-06-16 15:40:49 -0500

    agregar nuevas palabras mayusculas

--- scripst.js
+++ scripst.js
@@ -21,17 +21,58 @@ function imprimirvariables(){
 function arreglos(){
     let frutas= ['manzanas', 'peras', 'fresa'];
     //bucle
-    fort(let f of frutas){
+    for(let f of frutas){
         console.log(f)
     }
 
-    if (frutas[1]=== 'pera'  );
+    if (frutas[0] === 'pera'  ){
     console.log("La segunda fruta es una fresa");
-}else{
+} else {
+    console.log("No es una pera");
+}
     
 }
-console.log('No es una pera');
+document.getElementById("btnSaludar").addEventListener(
+    "click" , function () {
+        console.log("Hola!");
+    }
+);
+
+//modificar el html a traves del dom
+document.getElementById("btnAgregar") .addEventListener(
+    "click", () => {
+        //Obtener el elemento  "lista"
+        let lista = document.getElementById("lista");
+        let nuevoItem = docuement.createElement("li");
+        //obtener el total de items de la lita
+        let totalItems = lista.childElementCount;
+        //agregado un contenido(texto)
+        nuevoItem.textContect = 'Item $(totalItems + 1)';
+        lista.appendChild(nuevoItem);
+    }
+);
+
+     function addPalabraLista(){
+           console.log("Funciono!");
+
+}
+document,getElementById("btnAddPalabra").addEventListener(
+              "click", addPalabraLista
+
+);
+
+function addPalabraLista(){
+    let input= document.getElementById("txtEntrada");
+    const texto= input.value.trim();
+    console.log(texto);
+    const textMayuscula =texto.toUpperCase();
+    let lista = document.getElementById("lst palabras");
+    //creando  un nuevo elemento html 
+    let nuevoItem= document.createElement("li");
+    //agregando contenido
+    nuevoItem.textMayuscula = textMayuscula;
+    //agregar item a la lista 
+    lista.appendChild(nuevoItem);
 }
 
 
-ImprimirVariables();
